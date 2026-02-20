3db9f179bea25e7974e20670589c1ec340dd5274
David Kvistorf Larsen <dakl@itu.dk>
2026-02-20 15:50:38 +0100

    Update deploy.sh

--- Chirp-Project/deploy.sh
+++ Chirp-Project/deploy.sh
@@ -1,7 +1,7 @@
 source ~/.bash_profile
 
 # Make sure DOCKER_USERNAME is set
-export DOCKER_USERNAME=${DOCKER_USERNAME:-mydockerhubuser}
+export DOCKER_USERNAME=${DOCKER_USERNAME:-nickychengde}
 
 cd ../vagrant
 
