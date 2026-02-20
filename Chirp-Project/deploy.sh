a5c05b3b7ef617568efc87db9b53c03e4810058a
YeChengde <niye@itu.dk>
2026-02-20 14:45:17 +0100

    update path to docker compose in dpeloy-sh

--- Chirp-Project/deploy.sh
+++ Chirp-Project/deploy.sh
@@ -1,4 +1,4 @@
 source ~/.bash_profile
 
-docker compose -f docker-compose.yml pull
-docker compose -f docker-compose.yml up -d
+docker compose -f ../vagrant/docker-compose.yml pull
+docker compose -f ../vagrant/docker-compose.yml up -d
