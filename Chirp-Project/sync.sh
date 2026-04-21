eb799e4cea1253a70780975e236b6408d9697908
dakl <dakl@itu.dk>
2026-04-21 11:22:47 +0200

    small linting fix
    Co-Authored-By: Madsemil Søndergaard Søgaard <msso@itu.dk>

--- Chirp-Project/sync.sh
+++ Chirp-Project/sync.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 LB2_IP=$(cat ../vagrant/loadbalancer_ip.txt)
 rsync -avz -e "ssh -o StrictHostKeyChecking=no" /etc/letsencrypt/ root@$LB2_IP:/etc/letsencrypt/
 rsync -avz -e "ssh -o StrictHostKeyChecking=no"/etc/nginx/sites-available/veechirp.app root@$LB2_IP:/etc/nginx/sites-available/veechirp.app
